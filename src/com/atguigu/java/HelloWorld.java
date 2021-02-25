65015e61d80afc2e9675820f0dd48f207891e70c
LianOG <77385157@qq.com>
2021-02-25 14:24:47 +0800

    Initial commit

---
+++ src/com/atguigu/java/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.atguigu.java;
+
+import java.util.ArrayList;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        ArrayList list = new ArrayList();
+        System.out.println("HelloWorld!!");
+    }
+}
